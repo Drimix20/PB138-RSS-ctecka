8e0102a2fa7f4f83849dd0a20cbef6b8af170e8f
rzember <zemberrichard@gmail.com>
2015-05-07 22:17:35 +0200

    GUI aplikácie

--- RSS-Reader/src/main/java/pb138/rss/gui/ReaderUI.java
+++ RSS-Reader/src/main/java/pb138/rss/gui/ReaderUI.java
@@ -256,11 +256,7 @@ private void categorySelectorActionPerformed(java.awt.event.ActionEvent evt) {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        
         try {
             javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         } catch (ClassNotFoundException ex) {
