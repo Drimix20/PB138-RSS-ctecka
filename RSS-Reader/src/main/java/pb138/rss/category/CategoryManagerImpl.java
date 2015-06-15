619550ecad8a48ef4bf9e80c762b510621f563a1
mimaboci <michaelabocanova@gmail.com>
2015-06-16 00:14:08 +0200

    Update CategoryManagerImpl.java

--- RSS-Reader/src/main/java/pb138/rss/category/CategoryManagerImpl.java
+++ RSS-Reader/src/main/java/pb138/rss/category/CategoryManagerImpl.java
@@ -1,6 +1,7 @@
 package pb138.rss.category;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import pb138.rss.feed.RssFeed;
 import pb138.rss.feed.RssFeedContainer;
@@ -10,7 +11,7 @@ import pb138.rss.feed.RssFeedContainer;
  * @author Michaela
  */
 public class CategoryManagerImpl implements CategoryManager {
-    
+    //este doplnim
     private Set<Category> categories = new HashSet<>();
     
     public CategoryManagerImpl(Set<Category> categories) {
@@ -48,12 +49,15 @@ public class CategoryManagerImpl implements CategoryManager {
     }
 
     @Override
-    public RssFeedContainer showFeedsInCategory(RssFeedContainer container, Category category) {
+    public RssFeedContainer showFeedsInCategories(RssFeedContainer container, List<Category> categories) {
         RssFeedContainer filtered = new RssFeedContainer();
         for (String key : container.getKeys()) {
             RssFeed result = container.getFromFeedContainer(key);
-            if(result.getCategory().equals(category)) {
-                filtered.putIntoFeedContainer(key, result);
+            for (Category cat : categories) {
+                if(result.getCategory().equals(cat)) {
+                    filtered.putIntoFeedContainer(key, result);
+                    break;
+                }
             }
         }
         return filtered;     
