37387f54b5ec40408e8a9ee7d0762deb9512f36d
rzember <zemberrichard@gmail.com>
2015-06-22 21:11:40 +0200

    Update ConfigurationValidator.java

--- RSS-Reader/src/main/java/pb138/rss/configuration/ConfigurationValidator.java
+++ RSS-Reader/src/main/java/pb138/rss/configuration/ConfigurationValidator.java
@@ -6,17 +6,23 @@ import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
+import java.io.*;
+import org.apache.log4j.Logger;
 
 import org.xml.sax.SAXException;
+import pb138.rss.gui.ReaderUI;
 
 public class ConfigurationValidator {
     
+    private Logger logger = Logger.getLogger(ConfigurationLoader.class);
+    
     public static void main(String[] args){
+        File configuration = new File("target/configuration.xml");
         ConfigurationValidator validator = new ConfigurationValidator();
-        validator.validate();
+        validator.validate(configuration);
     }
 
-    public void validate() {
+    public void validate(File configuration) {
         try {  
             
             SchemaFactory schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
@@ -24,9 +30,9 @@ public class ConfigurationValidator {
             Schema sch = schemaFactory.newSchema(new StreamSource("src/main/java/pb138/rss/configuration/ConfigurationSchema.xsd"));
             Validator validator = sch.newValidator();
 
-            validator.validate(new StreamSource("src/main/java/pb138/rss/configuration/testconfig.xml")); //nazov suboru nie je finalny :)
+            validator.validate(new StreamSource(configuration)); 
 
-           //System.out.println("TEST FILE VALIDATED");
+            logger.info("Configuration file validated!");
 
         } catch (SAXException | IOException e) {            
             System.out.println("The configuration file is invalid: "+e.getMessage());
